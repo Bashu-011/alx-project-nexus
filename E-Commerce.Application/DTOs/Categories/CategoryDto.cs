e4935e549797560511b13e317a4c2ce0a38de267
Bashu-011 <bashu2609@gmail.com>
2025-11-25 15:52:36 +0300

    Added the category Dto

---
+++ E-Commerce.Application/DTOs/Categories/CategoryDto.cs
@@ -0,0 +1,13 @@
+﻿namespace E_Commerce.Application.DTOs.Categories;
+
+//the representation of a category that is sent to FE
+public class CategoryDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Slug { get; set; } = string.Empty;
+    public int ProductCount { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
