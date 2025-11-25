6b02ff05d1e2529dfdf480bc3e816bb402a719e7
Bashu-011 <bashu2609@gmail.com>
2025-11-25 14:41:24 +0300

    Updates to the User Repository

--- E-Commerce.Infrastructure/Repositories/UserRepository.cs
+++ E-Commerce.Infrastructure/Repositories/UserRepository.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using E_Commerce.Core.Entities;
+using E_Commerce.Infrastructure.Data;
+using ECommerce.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
-namespace E-Commerce.Infrastructure.Repositories
+namespace ECommerce.Infrastructure.Repositories;
+
+public class UserRepository : Repository<User>, IUserRepository
 {
-    internal class UserRepository
-{
-}
+    public UserRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        return await _dbSet.FirstOrDefaultAsync(u => u.Email == email);
+    }
 }
