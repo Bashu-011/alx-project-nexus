3daa6abec0c206254153ac04c16fcddb71b6147f
Bashu-011 <bashu2609@gmail.com>
2025-11-26 04:30:59 +0300

    Bug fix on E-Commerce Api - program.cs | Added mapping profile class for DTOs | Added Login and Registeration Validator | Product validator

---
+++ E-Commerce.API/Mappings/MappingProfile.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using E_Commerce.Application.DTOs.Auth;
+using E_Commerce.Application.DTOs.Categories;
+using E_Commerce.Application.DTOs.Products;
+using E_Commerce.Core.Entities;
+using Microsoft.AspNetCore.Identity.Data;
+
+namespace E_Commerce.Application.Mappings;
+
+//class that maps the dtos to the entities and vice versa
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        //User Mappings
+        CreateMap<User, AuthResponse>()
+            .ForMember(dest => dest.Token, opt => opt.Ignore())
+            .ForMember(dest => dest.ExpiresAt, opt => opt.Ignore());
+
+        CreateMap<RegisterRequest, User>()
+            .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.IsActive, opt => opt.Ignore())
+            .ForMember(dest => dest.Role, opt => opt.Ignore());
+
+        //Product Mappings
+        CreateMap<Product, ProductDto>()
+            .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name));
+
+        CreateMap<CreateProductRequest, Product>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.Slug, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => true))
+            .ForMember(dest => dest.Category, opt => opt.Ignore());
+
+        CreateMap<UpdateProductRequest, Product>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.Slug, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.Category, opt => opt.Ignore());
+
+        //Category Mappings
+        CreateMap<Category, CategoryDto>()
+            .ForMember(dest => dest.ProductCount, opt => opt.MapFrom(src => src.Products.Count));
+
+        CreateMap<CreateCategoryRequest, Category>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.Slug, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.Products, opt => opt.Ignore());
+
+        CreateMap<UpdateCategoryRequest, Category>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.Slug, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.Products, opt => opt.Ignore());
+    }
+}
---
+++ E-Commerce.API/Validators/Product/ProductRequestValidator.cs
@@ -0,0 +1,31 @@
+﻿using E_Commerce.Application.DTOs.Products;
+using FluentValidation;
+
+namespace E_Commerce.Application.Validators.Products;
+
+public class CreateProductRequestValidator : AbstractValidator<CreateProductRequest>
+{
+    public CreateProductRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Product name is required")
+            .MaximumLength(200).WithMessage("Product name must not exceed 200 characters");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required")
+            .MaximumLength(2000).WithMessage("Description must not exceed 2000 characters");
+
+        RuleFor(x => x.Price)
+            .GreaterThan(0).WithMessage("Price must be greater than 0")
+            .LessThan(1000000).WithMessage("Price must be less than 1,000,000");
+
+        RuleFor(x => x.StockQuantity)
+            .GreaterThanOrEqualTo(0).WithMessage("Stock quantity cannot be negative");
+
+        RuleFor(x => x.ImageUrl)
+            .NotEmpty().WithMessage("Image URL is required");
+
+        RuleFor(x => x.CategoryId)
+            .NotEmpty().WithMessage("Category is required");
+    }
+}
