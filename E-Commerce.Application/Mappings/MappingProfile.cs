ed0e040d5e7eea8edfdf627c5a44416c05989912
Bashu-011 <bashu2609@gmail.com>
2025-11-28 11:46:34 +0300

    Syntax error fixes on automapper

--- E-Commerce.Application/Mappings/MappingProfile.cs
+++ E-Commerce.Application/Mappings/MappingProfile.cs
@@ -19,7 +19,7 @@ public class MappingProfile : Profile
             .ForMember(dest => dest.Token, opt => opt.Ignore())
             .ForMember(dest => dest.ExpiresAt, opt => opt.Ignore())
             .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role.ToString()));
-        The winner 
+     
         CreateMap<RegisterationRequest, User>()
             .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
             .ForMember(dest => dest.Id, opt => opt.Ignore())
